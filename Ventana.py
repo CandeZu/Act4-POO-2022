d0b82889a944dc6dd1b20be8a4a4307e66222fef
CandeZ <candelazumel@gmail.com>
2022-05-02 18:00:08 -0300

    def atributos

---
+++ Ventana.py
@@ -0,0 +1,15 @@
+class Ventana:
+    __titulo = ""
+    __xsupizq = 0
+    __ysupizq = 0
+    __xinfder = 0
+    __yinfder = 0
+
+    def __init__(self, titulo, xsupizq, ysupizq, xinfder, yinfder):
+        self.__titulo = titulo
+        self.__xsupizq = xsupizq
+        self.__ysupizq = ysupizq
+        self.__xinfder = xinfder
+        self.__yinfder = yinfder
+    
+    
