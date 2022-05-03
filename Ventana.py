7a75727cd3db11e735b8a812e701cfc12e4cc838
CandeZ <candelazumel@gmail.com>
2022-05-02 22:38:23 -0300

    ej4 terminado

--- Ventana.py
+++ Ventana.py
@@ -5,11 +5,46 @@ class Ventana:
     __xinfder = 0
     __yinfder = 0
 
-    def __init__(self, titulo, xsupizq, ysupizq, xinfder, yinfder):
+    def __init__(self, titulo, xsupizq=0, ysupizq=0, xinfder=500, yinfder=500):
         self.__titulo = titulo
         self.__xsupizq = xsupizq
         self.__ysupizq = ysupizq
         self.__xinfder = xinfder
         self.__yinfder = yinfder
     
+    def mostrar(self):
+        print("{} \nCoordenadas vertice inferior: ({},{}) \nCoordenadas vertice superior: ({},{})\n".format(self.__titulo, self.__xinfder, self.__yinfder, self.__xsupizq, self.__ysupizq))
+
+    def alto(self):
+        return self.__yinfder - self.__ysupizq
+
+    def ancho(self):
+        return self.__xinfder - self.__xsupizq
     
+    def getTitulo(self):
+        return self.__titulo
+    
+    def moverDerecha(self, x=10):
+        if (self.__xinfder <500) and (self.__xsupizq > 0):
+            self.__xsupizq += x
+            self.__xinfder += x
+        
+    def moverIzquierda(self, x = 10):
+        if (self.__xinfder <500) and (self.__xsupizq > 0):
+            self.__xsupizq -= x
+            self.__xinfder -= x
+        
+    def bajar(self,y = 10):
+        if (self.__yinfder <500) and (self.__ysupizq > 0):
+            self.__ysupizq += y
+            self.__yinfder += y
+    
+    def subir (self,y= 10):
+        if (self.__yinfder <500) and (self.__ysupizq > 0):
+            self.__ysupizq -= y
+            self.__yinfder -= y
+    
+
+
+
+
